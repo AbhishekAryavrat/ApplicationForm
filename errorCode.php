f3795a808f90be2f53feec8c5b016a129450e902
AkMANZHI <abhishek.manjhi@aryavratinfotech.com>
2019-08-03 18:04:31 +0530

    WIP

---
+++ errorCode.php
@@ -0,0 +1,41 @@
+next_step1 {
+	101=Name box Empty;
+	102=Name Format was Incorrect;
+	103=Father Name box Empty ;
+	104=Father name format is Incorrect;
+	105=Mothername Name box Empty;
+	106=Mother name format is Incorrect;
+	107=Fill the Radio Buttons(Gender);
+	108=Pincode Box is empty;
+	109=Pincode format was Incorrect;
+	110=Fill The Email Box;
+	111=Email Format  was Incorrect;
+}
+
+
+next_step2{
+	112=Fill Senior percentage;
+	113=Incorrect Senior Percentage;
+	114=Fill the Senior Board;
+	115=Incorrect Board of Senior(10th);
+	116=Fill Senior Secondary percentage;
+	117=Incorrect Senior Secondary Percentage;
+	118=Fill the Senior Secondary Board;
+	119=Incorrect Board of SeniorSecondary(12th);
+	120=Fill University percentage;
+	121=Incorrect University Percentage;
+	122=Fill the University Box;
+	123=Incorrect University Format;
+
+}
+
+function showPreview(userImage){
+	124=Image is Required;
+	125=Please Select a Valid Image(Only jpeg, jpg Images);
+
+}
+
+function showPreview(Siganture_Image){
+	126=Image is Required;
+	127=Please Select a Valid Image(Only jpeg, jpg Images);
+}
