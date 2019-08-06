063d659d3e64665bbe4feb766e8d6439fde2bce0
AkMANZHI <abhishek.manjhi@aryavratinfotech.com>
2019-08-06 17:51:39 +0530

    WIP

--- demo.php
+++ demo.php
@@ -25,18 +25,64 @@
 				<h2>Personal details</h2>
 				<div>
 					<label for ="userName">Name:</label>
-					<input type="text" name="userName" id="userName"  onchange="nameValidate(value);">
-					<p id="unameValidate"></p>
+					<input type="text" name="userName" id="user_Name"  onchange="nameValidate(name);">
+					<p name = "valid" id="unameValidate"></p>
 					
 					<label for ="fatherName">Father Name:</label>
-					<input type="text" name="fatherName" id="fatherName" onchange="nameValidate(value);">
-					<p id= "fNameValidate"></p>
+					<input type="text" name="fatherName" id="father_Name" onchange="nameValidate(name);">
+					<p name = "valid" id= "fNameValidate"></p>
 
 					<label for ="motherName">Mother Name:</label>
-					<input type="text" name="motherName" id="motherName" onchange="nameValidate(value);">
-					<p id= "mNameValidate"></p>
+					<input type="text" name="motherName" id="mother_Name" onchange="nameValidate(name);">
+					<p name = "valid" id= "mNameValidate"></p>
+
+					<label for="gender">Gender:
+						<input type="radio" name="gender" id=male value="male">Male
+						<input type="radio" name="gender" id= female  value= "female">Female
+						<br>
+					</label>
+					<p id= "genderValidation"></p>
+
+					<label>Address:</label>
+					<textarea name="Address" rows="5" cols="15"></textarea>
+					<br><br>
+
+					<label for="pincode">Pincode</label>
+					<input type="text" name="pincode" id="_pincode" onchange="Validate(name);">
+					<p name = "valid" id="pincodeValidation"></p>
+
+					<label for="email">Email ID</label>
+					<input type="text" name="email" id="_email" onchange="Validate(name);">
+					<p name = "valid" id="emailValidation">
 				</div>
+				<input type="button" id="next_btn1" onclick="nextStep();" value="Next">
+
+
 		</div>
+		<div class="second" id="2">
+			
+			<!-- create Educational Details box-->
+			<h2>Educational Details</h2>
+				<p>Senior(10th%)<input type="text" name="senior_percentage" placeholder="Percentage" id="senior_percentage" ></p>
+				<p>Board:<input type="text" name="seniorBoard"  onkeyup="board(this)" value="" id="seniorBoard"></p>
+				<p>Senior Seconadry(12th%):<input type="text" name="secondary_percentage" placeholder="Percentage" id="secondary_percentage"></p>
+				<p>Board:<input type="text" name="secondaryBoard" onkeyup="board(this)" value="" id="secondaryBoard"></p>
+			
+					<label>Graduation Course:</label>
+					<!--create drop down list-->
+					<select name="graduation_course">
+						<option value="BCA">BCA</option>
+						<option value="BBA">BBA</option>
+						<option value="BBA">B.COM</option>
+						<option value="BBA">B.A</option>
+						<option value="BBA">B.TECH</option>			
+					</select>
+						<p>Percentage:<input type="text" name="university_percentage" id="university_percentage" placeholder="Percentage">
+						<p>University/Board:<input type="text" name="university"  onkeyup="board(this)" value="" id="university"><br>
+						<input type="button" name="Previous1" onclick="pre_step1()" id="pre_btn1" value="Previous step">
+						<input type="button" name="Next" onclick="next_step2()" id="next_btn2" value="Next step">		
+		</div>
+
 
 
 
--- javascript/demo.js
+++ javascript/demo.js
@@ -1,39 +1,223 @@
+// 
+
+// var name = "",count=1;
+
+// function nameValidate(info)
+// {
+// 	if(info == 'userName'){nameValidation(info,"unameValidate","Name");}
+// 	if(info == 'fatherName'){nameValidation(info,"fNameValidate","Father Name");}
+// 	if(info == 'motherName'){nameValidation(info,"mNameValidate","Mother Name");}
+// }
+
+// function nameValidation(info,errDis,errName)
+// {
+// 	try{
+			
+// 			let name = document.getElementsByName(info)[0].value;
+
+// 			if(name == 0) 
+// 				{throw "Field name can not be empty";} 
+// 			else{errDis.innerHTML = "";trydemo(errDis);}
+
+// 				let val = name.trim();
+// 				let len = val.length;
+
+// 				if(len < 2) {throw "Value is too low";}else{document.getElementById(errDis).innerHTML = "";trydemo(errDis);}
+// 				if(len > 15) {throw "Value is too High";}else{document.getElementById(errDis).innerHTML = "";trydemo(errDis);}
+
+// 				let alpha = /^[a-zA-Z\s]+$/;
+// 				if(!name.match(alpha)) {throw "Value should be only Alphabets";}
+// 				else{document.getElementById(errDis).innerHTML = "";trydemo(errDis);}
+
+				
+			
+// 		}
+// 	catch(err)
+// 	{
+// 		document.getElementById(errDis).innerHTML = errName + " " + err;
+// 		trydemo(errDis);
+// 	}
+// }
+
+
+// function Validate(info)
+// {	
+// 	let regEx1 = /^\d{6}$/;
+// 	let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+// 	if(info === 'email'){emaPinValid(info,"emailValidation","Email ID",regEx);}
+// 	if(info === 'pincode'){emaPinValid(info,"pincodeValidation","Pincode",regEx1);}
+// }
+
+// function emaPinValid (info,errDis,errName,expr)
+// {
+// 	let name = document.getElementsByName(info)[0].value;
+// 	try
+// 	{if(!name.match(expr)){throw "Incorrect Format of ";}
+// 	else{document.getElementById(errDis).innerHTML = "";trydemo(errDis);}}
+	
+// 	catch(err)
+// 	{
+// 		document.getElementById(errDis).innerHTML = err + errName;
+// 		trydemo(errDis);
+// 	}
+// }
+
+// function trydemo(errDis)
+// {
+// 	// var value1 = document.getElementById(errDis).textContent;
+// 	if(document.getElementById(errDis).textContent == ""){count = 0;}
+// 	else{count++;}
+// }
+
+
+// function genderValidate()
+// {
+// 	var radio_check = document.getElementsByName("gender");
+// 	let errDis = "genderValidation";
+// 	var genCount =1;
+// 	try{if(radio_check[0].checked == false && radio_check[1].checked == false ){genCount++; throw "Fill the Gender Buttons";}
+// 		else{document.getElementById(errDis).innerHTML = "";genCount = 0;}
+// 		}
+// 	catch(err)
+// 	{
+// 		document.getElementById(errDis).innerHTML = err ;
+// 	}
+
+// 	return genCount;
+// }
+
+// function nextStep()
+// {
+
+// 	var genCount = genderValidate();
+
+// 	if(count == 0 && genCount == 0)
+// 	{
+// 		document.getElementById('1').style.display = "block";
+// 		document.getElementById('2').style.display = "block";
+// 		document.getElementById('3').style.display = "none";
+// 	}
+// }
+
+
+
+var name = "",count=1,validCount=0;
+
 function nameValidate(info)
 {
-	var uName = document.getElementById('unameValidate');
-	var fName = document.getElementById('fNameValidate');
-	var mName = document.getElementById('mNameValidate');
-
-	var userName = document.getElementById('userName').value;
-	var fatherName = document.getElementById('fatherName').value;
-	var motherName = document.getElementById('motherName').value;
-
-	if(userName === info){nameValidation(info,uName,"Name");}
-	if(fatherName === info){nameValidation(info,fName,"Father Name");}
-	if(motherName === info){nameValidation(info,mName,"Mother Name");}
+	if(info == 'userName'){nameValidation(info,"unameValidate","Name");}
+	if(info == 'fatherName'){nameValidation(info,"fNameValidate","Father Name");}
+	if(info == 'motherName'){nameValidation(info,"mNameValidate","Mother Name");}
 }
 
 function nameValidation(info,errDis,errName)
 {
 	try{
+			
+			let name = document.getElementsByName(info)[0].value;
 
-			if(info == 0) 
+			if(name == 0) 
 				{throw "Field name can not be empty";} 
 			else{errDis.innerHTML = "";}
 
-				let val = info.trim();
+				let val = name.trim();
 				let len = val.length;
 
-				if(len < 2) {throw "Value is too low";}else{errDis.innerHTML = "";}
-				if(len > 15) {throw "Value is too High";}else{errDis.innerHTML = "";}
+				if(len < 2) {throw "Value is too low";}else{document.getElementById(errDis).innerHTML = "";}
+				if(len > 15) {throw "Value is too High";}else{document.getElementById(errDis).innerHTML = "";}
 
 				let alpha = /^[a-zA-Z\s]+$/;
-				if(!info.match(alpha)) {throw "Value should be only Alphabets";}else{errDis.innerHTML = "";}
+				if(!name.match(alpha)) {throw "Value should be only Alphabets";}
+				else{document.getElementById(errDis).innerHTML = "";}
+
+				
 			
 		}
-
 	catch(err)
 	{
-		errDis.innerHTML = errName + " " + err;
+		document.getElementById(errDis).innerHTML = errName + " " + err;
+		
 	}
 }
+
+
+function Validate(info)
+{	
+	let regEx1 = /^\d{6}$/;
+	let regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+	if(info === 'email'){emaPinValid(info,"emailValidation","Email ID",regEx);}
+	if(info === 'pincode'){emaPinValid(info,"pincodeValidation","Pincode",regEx1);}
+}
+
+function emaPinValid (info,errDis,errName,expr)
+{
+	let name = document.getElementsByName(info)[0].value;
+	try
+	{if(!name.match(expr)){throw "Incorrect Format of ";}
+	else{document.getElementById(errDis).innerHTML = "";}}
+	
+	catch(err)
+	{
+		document.getElementById(errDis).innerHTML = err + errName;
+		
+	}
+}
+
+function genderValidate()
+{
+	var radio_check = document.getElementsByName("gender");
+	let errDis = "genderValidation";
+	var genCount =1;
+	try{if(radio_check[0].checked == false && radio_check[1].checked == false ){genCount++; throw "Fill the Gender Buttons";}
+		else{document.getElementById(errDis).innerHTML = "";genCount = 0;}
+		}
+	catch(err)
+	{
+		document.getElementById(errDis).innerHTML = err ;
+	}
+
+	return genCount;
+}
+
+
+function nextStep()
+{
+
+	var genResult = genderValidate();
+	var result = countVal();
+	if(result == 5){count = 0;validCount=0;}else{validCount = 0;}
+
+	if(genResult == 0 && count == 0)
+	{
+		document.getElementById('1').style.display = "block";
+		document.getElementById('2').style.display = "block";
+		document.getElementById('3').style.display = "none";
+	}
+	else
+	{
+		// document.getElementById('1').style.display = "block";
+		// document.getElementById('2').style.display = "none";
+		// document.getElementById('3').style.display = "none";
+		alert("Please Fill the All Details");
+	}
+}
+
+function countVal()
+{
+	var x = document.getElementsByName('valid').length;
+	for(let i =0;i<x;i++)
+	{
+		if(document.getElementsByName('valid')[i].textContent == "")
+		{
+			validCount++;
+		}
+	}
+
+	return validCount;
+}
+
+
+
+		// document.getElementById('1').style.display = "block";
+		// document.getElementById('2').style.display = "block";
+		// document.getElementById('3').style.display = "none";
